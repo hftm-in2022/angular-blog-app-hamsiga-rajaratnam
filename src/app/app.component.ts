4aad9f5f5d6bbf73527fe271c909a0bef7dbbb98
Hamsiga Rajaratnam <hamsiga.rajaratnam@ionesoft.ch>
2024-11-01 12:01:41 +0100

    feat: todo demo app (not working) added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-app';
+  title = 'Todo App';
+  todos: { text: string, completed: boolean }[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo) {
+      this.todos.push({ text: this.newTodo, completed: false });
+      this.newTodo = '';
+    }
+  }
+
+  toggleTodoCompletion(todo: { text: string, completed: boolean }) {
+    todo.completed = !todo.completed;
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
+// app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop'), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } } // Set default appearance
+  ],
 };
