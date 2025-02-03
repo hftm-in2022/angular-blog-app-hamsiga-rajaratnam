e06e86bb76de5f907d4c9a6e1cc56b96103bd0bd
Hamsiga Rajaratnam <hamsiga.rajaratnam@ionesoft.ch>
2025-02-03 12:32:07 +0100

    feat(i18n): added translation files for DE and EN

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {provideHttpClient, HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import { provideAuth } from 'angular-auth-oidc-client';
 import { authConfig } from './auth/auth.config';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +20,14 @@ export const appConfig: ApplicationConfig = {
     provideAuth(authConfig), // OIDC Client
     { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }, // Logging Interceptor
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }, // Error Interceptor
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
   ],
 };
